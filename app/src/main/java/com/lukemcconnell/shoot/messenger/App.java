f8266e69e2d45343fc0171ccc642b731ec051bc5
Luke McConnell <lukermcconnell@gmail.com>
2022-02-03 05:13:03 +0000

    Create terminal menu

--- app/src/main/java/com/lukemcconnell/shoot/messenger/App.java
+++ app/src/main/java/com/lukemcconnell/shoot/messenger/App.java
@@ -1,17 +1,40 @@
 /*
-  Lukes-Mac-mini.local
-  java EchoServer 7
-  java EchoClient echoserver.example.com 7
+  Shoot Messenger
+  Luke McConnell
 */
+
 package com.lukemcconnell.shoot.messenger;
 
+import java.util.Scanner; 
 public class App {
     public String getGreeting() {
         return "Welcome to Shoot Messenger!";
     }
 
+    public static String[] getUserInputStr() {
+        String[] varNames = {"username", "mode"};
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        String[] userInputArr = new String[2];
+        userInputArr[0] = null;
+        userInputArr[1] = null;
+        for (int i = 0; i < varNames.length; i++) {
+            System.out.println("Enter " + varNames[i] + ": ");
+            while(userInputArr[i] == null){
+                if (scanner.hasNextLine()){
+                    userInputArr[i] = scanner.nextLine();
+                }
+            }
+        } 
+        scanner.close();
+        return userInputArr;
+      }
+
     public static void main(String[] args) {
         App messenger = new App();
         System.out.println(messenger.getGreeting());
+        String[] userInputArr = getUserInputStr();
+        String username =  userInputArr[0];
+        String mode =  userInputArr[1];
+        System.out.println(username + " selected " + mode + " mode!");
     }
 }
